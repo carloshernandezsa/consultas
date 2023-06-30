d2c209bc0aa98c3182e630acc07130dbac6d4000
Lucia Aranda <lucyarandal@gmail.com>
2023-06-29 19:20:24 -0600

    Primer entrega

--- content/js/consultas-abogados-registrados.js
+++ content/js/consultas-abogados-registrados.js
@@ -4,114 +4,137 @@
 // Cargar previemante
 // - consultas-api-url.js
 //
+document.addEventListener("DOMContentLoaded", function() {
 
-// Definir elementos del DOM
-const abogadosRegistradosFormCard = document.getElementById("abogadosRegistradosFormCard");
-const abogadosRegistradosFormSpinner = document.getElementById("abogadosRegistradosFormSpinner");
-const abogadosRegistradosForm = document.getElementById("abogadosRegistradosForm");
-const abogadosRegistradosTableCard = document.getElementById("abogadosRegistradosTableCard");
-const abogadosRegistradosTableTitle = document.getElementById("abogadosRegistradosTableTitle");
-const abogadosRegistradosTableSpinner = document.getElementById("abogadosRegistradosTableSpinner");
-const abogadosRegistradosTable = document.getElementById("abogadosRegistradosTable");
-const nombreInput = document.getElementById("nombreInput");
-const anioDesdeInput = document.getElementById("anioDesdeInput");
-const anioHastaInput = document.getElementById("anioHastaInput");
-const consultarButton = document.getElementById("consultarButton");
+  // Definir URL sin parámetros
+  const actualUrl = window.location.href.split("?")[0];
 
-// Consultar los abogados registrados para llenar la tabla
-function consultarAbogadosRegistrados(nombre, anioDesde, anioHasta) {
-  abogadosRegistradosTableSpinner.style.display = "block";
-  if (nombre == null) {
-    nombre = "";
-  }
-  if (anioDesde == null || anioDesde == "") {
-    anioDesde = "1925";
-  }
-  if (anioHasta == null || anioHasta == "") {
-    anioHasta = new Date().getFullYear();
-  }
-  abogadosRegistradosTableTitle.innerHTML = "Resultados con nombre '" + nombre + "' entre " + anioDesde + " y " + anioHasta;
-  $("#abogadosRegistradosTable").DataTable({
-    lengthChange: false,
-    ordering: false,
-    searching: false,
-    scrollX: true,
-    serverSide: true,
-    ajax: {
-      url: apiUrl + "/abogados/datatable",
-      headers: { "X-Api-Key": apiKey },
-      data: {
-        nombre: nombre,
-        anio_desde: anioDesde,
-        anio_hasta: anioHasta,
+  // Definir elementos del DOM
+  const abogadosRegistradosFormCard = document.getElementById("abogadosRegistradosFormCard");
+  const abogadosRegistradosFormSpinner = document.getElementById("abogadosRegistradosFormSpinner");
+  const abogadosRegistradosForm = document.getElementById("abogadosRegistradosForm");
+  const abogadosRegistradosTableCard = document.getElementById("abogadosRegistradosTableCard");
+  const abogadosRegistradosTableTitle = document.getElementById("abogadosRegistradosTableTitle");
+  const abogadosRegistradosTableSpinner = document.getElementById("abogadosRegistradosTableSpinner");
+  const abogadosRegistradosTable = document.getElementById("abogadosRegistradosTable");
+  const nombreInput = document.getElementById("nombreInput");
+  const anioDesdeInput = document.getElementById("anioDesdeInput");
+  const anioHastaInput = document.getElementById("anioHastaInput");
+  const consultarButton = document.getElementById("consultarButton");
+  const cambiarDatosButton = document.getElementById("cambiarDatosButton");
+
+  // Consultar los abogados registrados para llenar la tabla
+  function consultarAbogadosRegistrados(nombre, anioDesde, anioHasta) {
+    abogadosRegistradosTableSpinner.style.display = "block";
+    if (nombre == null) {
+      nombre = "";
+    }
+    if (anioDesde == null || anioDesde == "") {
+      anioDesde = "1925";
+    }
+    if (anioHasta == null || anioHasta == "") {
+      anioHasta = new Date().getFullYear();
+    }
+    abogadosRegistradosTableTitle.innerHTML = "Resultados con nombre '" + nombre + "' entre " + anioDesde + " y " + anioHasta;
+    $("#abogadosRegistradosTable").DataTable({
+      lengthChange: false,
+      ordering: false,
+      searching: false,
+      scrollX: true,
+      serverSide: true,
+      ajax: {
+        url: apiUrl + "/abogados/datatable",
+        headers: { "X-Api-Key": apiKey },
+        data: {
+          nombre: nombre,
+          anio_desde: anioDesde,
+          anio_hasta: anioHasta,
+        },
+        type: "GET",
+        dataType: "json",
       },
-      type: "GET",
-      dataType: "json",
-    },
-    columns: [
-      { data: "fecha", width: "10%" },
-      { data: "libro", width: "10%" },
-      { data: "numero", width: "10%" },
-      { data: "nombre", width: "60%" },
-    ],
-    columnDefs: [
-      {
-        targets: 0,
-        data: null,
-        render: function (data, type, row) {
-          return moment(data).format("DD/MMM/YYYY");
+      columns: [
+        { data: "fecha", width: "10%" },
+        { data: "libro", width: "10%" },
+        { data: "numero", width: "10%" },
+        { data: "nombre", width: "60%" },
+      ],
+      columnDefs: [
+        {
+          targets: 0,
+          data: null,
+          render: function (data, type, row) {
+            return moment(data).format("DD/MMM/YYYY");
+          },
+        },
+      ],
+      language: {
+        lengthMenu: "Mostrar _MENU_",
+        search: "Filtrar:",
+        zeroRecords: "No hay registros.",
+        info: "Página _PAGE_ de _PAGES_",
+        infoEmpty: "No hay registros",
+        infoFiltered: "(filtrados desde _MAX_ registros totales)",
+        oPaginate: {
+          sFirst: "Primero",
+          sLast: "Último",
+          sNext: "Siguiente",
+          sPrevious: "Anterior",
         },
       },
-    ],
-    language: {
-      lengthMenu: "Mostrar _MENU_",
-      search: "Filtrar:",
-      zeroRecords: "No hay registros.",
-      info: "Página _PAGE_ de _PAGES_",
-      infoEmpty: "No hay registros",
-      infoFiltered: "(filtrados desde _MAX_ registros totales)",
-      oPaginate: {
-        sFirst: "Primero",
-        sLast: "Último",
-        sNext: "Siguiente",
-        sPrevious: "Anterior",
-      },
-    },
+    });
+    abogadosRegistradosTableSpinner.style.display = "none";
+  }
+
+  // Recargar la pagina esta página sin parámetros
+  function recargarSinParametros() {
+    window.location.href = actualUrl;
+  }
+  // Recargar la pagina con los parametros del formulario
+  function recargarConParametros() {
+    const actualUrl = window.location.href.split("?")[0];
+    const elNombre = nombreInput.value;
+    const elAnioDesde = anioDesdeInput.value;
+    const elAnioHasta = anioHastaInput.value;
+    window.location.href = actualUrl + "?nombre=" + elNombre + "&anio_desde=" + elAnioDesde + "&anio_hasta=" + elAnioHasta;
+  }
+
+  // Limpiar los valores del formulario y regresar a consultar
+  function replyConsulta(){
+    nombreInput.value = "";
+    anioDesdeInput.value = "";
+    anioHastaInput.value = "";
+
+    recargarSinParametros();
+  }
+  //
+  // Proceso inicial
+  //
+
+  // Obtener los parametros de la URL
+  const urlParams = new URLSearchParams(window.location.search);
+  var nombre = urlParams.get("nombre");
+  var anioDesde = urlParams.get("anio_desde");
+  var anioHasta = urlParams.get("anio_hasta");
+
+  // Si se recibio por lo menos un parametro
+  if (nombre != null || anioDesde != null || anioHasta != null) {
+    // Mostrar el card con la tabla DataTable
+    abogadosRegistradosFormCard.style.display = "none";
+    abogadosRegistradosTableCard.style.display = "block";
+    consultarAbogadosRegistrados(nombre, anioDesde, anioHasta);
+  } else {
+    // Mostrar el card con el formulario para consultar
+    abogadosRegistradosFormCard.style.display = "block";
+    abogadosRegistradosFormSpinner.style.display = "none";
+    abogadosRegistradosForm.style.display = "block";
+    consultarButton.addEventListener("click", (thisEvent) => {
+      recargarConParametros();
+    });
+  }
+
+  cambiarDatosButton.addEventListener("click", (thisEvent) => {
+    encabezadoDiv.style.display = "block";
+    replyConsulta();
   });
-  abogadosRegistradosTableSpinner.style.display = "none";
-}
-
-// Recargar la pagina con los parametros del formulario
-function recargarConParametros() {
-  const actualUrl = window.location.href.split("?")[0];
-  const elNombre = nombreInput.value;
-  const elAnioDesde = anioDesdeInput.value;
-  const elAnioHasta = anioHastaInput.value;
-  window.location.href = actualUrl + "?nombre=" + elNombre + "&anio_desde=" + elAnioDesde + "&anio_hasta=" + elAnioHasta;
-}
-
-//
-// Proceso inicial
-//
-
-// Obtener los parametros de la URL
-const urlParams = new URLSearchParams(window.location.search);
-var nombre = urlParams.get("nombre");
-var anioDesde = urlParams.get("anio_desde");
-var anioHasta = urlParams.get("anio_hasta");
-
-// Si se recibio por lo menos un parametro
-if (nombre != null || anioDesde != null || anioHasta != null) {
-  // Mostrar el card con la tabla DataTable
-  abogadosRegistradosFormCard.style.display = "none";
-  abogadosRegistradosTableCard.style.display = "block";
-  consultarAbogadosRegistrados(nombre, anioDesde, anioHasta);
-} else {
-  // Mostrar el card con el formulario para consultar
-  abogadosRegistradosFormCard.style.display = "block";
-  abogadosRegistradosFormSpinner.style.display = "none";
-  abogadosRegistradosForm.style.display = "block";
-  consultarButton.addEventListener("click", (thisEvent) => {
-    recargarConParametros();
-  });
-}
+});
--- content/js/consultas-repsvm.js
+++ content/js/consultas-repsvm.js
@@ -3,97 +3,127 @@
 //
 // Cargar previemante
 // - consultas-api-url.js
+// - consultas-distritos.js
 //
 
-// Definir elementos del DOM
-const repsvmFormCard = document.getElementById("repsvmFormCard");
-const repsvmFormSpinner = document.getElementById("repsvmFormSpinner");
-const repsvmForm = document.getElementById("repsvmForm");
-const repsvmTableCard = document.getElementById("repsvmTableCard");
-const repsvmTableTitle = document.getElementById("repsvmTableTitle");
-const repsvmTableSpinner = document.getElementById("repsvmTableSpinner");
-const repsvmTable = document.getElementById("repsvmTable");
-const nombreInput = document.getElementById("nombreInput");
-const consultarButton = document.getElementById("consultarButton");
-
-// Consultar el repsvm para llenar la tabla
-function consultarRepsvm(nombre) {
-  repsvmTableSpinner.style.display = "block";
-  if (nombre == null) {
-    nombre = "";
-  }
-  repsvmTableTitle.innerHTML = "Con nombre " + nombre;
-  $("#repsvmTable").DataTable({
-    lengthChange: false,
-    ordering: false,
-    searching: false,
-    scrollX: true,
-    serverSide: true,
-    ajax: {
-      url: apiUrl + "/repsvm_agresores/datatable",
-      headers: { "X-Api-Key": apiKey },
-      type: "GET",
-      data: {
-        nombre: nombre,
-      },
-      dataType: "json",
-    },
-    columns: [
-      { data: "distrito_nombre_corto", width: "10%" },
-      { data: "nombre", width: "20%" },
-      { data: "tipo_juzgado", width: "10%" },
-      { data: "delito_generico", width: "10%" },
-      { data: "delito_especifico", width: "10%" },
-      { data: "tipo_sentencia", width: "10%" },
-      { data: "numero_causa", width: "10%" },
-      { data: "pena_impuesta", width: "10%" },
-      { data: "observaciones", width: "10%" },
-    ],
-    language: {
-      lengthMenu: "Mostrar _MENU_",
-      search: "Filtrar:",
-      zeroRecords: "No hay registros.",
-      info: "Página _PAGE_ de _PAGES_",
-      infoEmpty: "No hay registros",
-      infoFiltered: "(filtrados desde _MAX_ registros totales)",
-      oPaginate: {
-        sFirst: "Primero",
-        sLast: "Último",
-        sNext: "Siguiente",
-        sPrevious: "Anterior",
-      },
-    },
-  });
-  repsvmTableSpinner.style.display = "none";
-}
-
-// Recargar la pagina con los parametros del formulario
-function recargarConParametros() {
+document.addEventListener("DOMContentLoaded", function() {
+  // Definir URL sin parámetros
   const actualUrl = window.location.href.split("?")[0];
-  const elNombre = nombreInput.value;
-  window.location.href = actualUrl + "?nombre=" + elNombre;
-}
 
-//
-// Proceso inicial
-//
+  // Definir elementos del DOM
+  const repsvmFormCard = document.getElementById("repsvmFormCard");
+  const repsvmFormSpinner = document.getElementById("repsvmFormSpinner");
+  const repsvmForm = document.getElementById("repsvmForm");
+  const repsvmTableCard = document.getElementById("repsvmTableCard");
+  const repsvmTableTitle = document.getElementById("repsvmTableTitle");
+  const repsvmTableSpinner = document.getElementById("repsvmTableSpinner");
+  const repsvmTable = document.getElementById("repsvmTable");
+  const nombreInput = document.getElementById("nombreInput");
+  const consultarButton = document.getElementById("consultarButton");
+  const regresarButton = document.getElementById("regresarButton");
 
-// Obtener los parametros de la URL
-const urlParams = new URLSearchParams(window.location.search);
-var nombre = urlParams.get("nombre");
+  // Consultar el repsvm para llenar la tabla
+  function consultarRepsvm(nombre, distritoClave) {
+    repsvmTableSpinner.style.display = "block";
+    if (nombre == null) {
+      nombre = "";
+    }
+    if (distritoClave == null) {
+      distritoClave = "";
+    }
+    repsvmTableTitle.innerHTML = "Con nombre " + nombre + " con clave del distrtito " + distritoClave;
+    $("#repsvmTable").DataTable({
+      lengthChange: false,
+      ordering: false,
+      searching: false,
+      scrollX: true,
+      serverSide: true,
+      ajax: {
+        url: apiUrl + "/repsvm_agresores/datatable",
+        headers: { "X-Api-Key": apiKey },
+        type: "GET",
+        data: {
+          nombre: nombre,
+          distrito_clave:distritoClave,
+        },
+        dataType: "json",
+      },
+      columns: [
+        { data: "distrito_nombre_corto", width: "10%" },
+        { data: "nombre", width: "20%" },
+        { data: "tipo_juzgado", width: "10%" },
+        { data: "delito_generico", width: "10%" },
+        { data: "delito_especifico", width: "10%" },
+        { data: "tipo_sentencia", width: "10%" },
+        { data: "numero_causa", width: "10%" },
+        { data: "pena_impuesta", width: "10%" },
+        { data: "observaciones", width: "10%" },
+      ],
+      language: {
+        lengthMenu: "Mostrar _MENU_",
+        search: "Filtrar:",
+        zeroRecords: "No hay registros.",
+        info: "Página _PAGE_ de _PAGES_",
+        infoEmpty: "No hay registros",
+        infoFiltered: "(filtrados desde _MAX_ registros totales)",
+        oPaginate: {
+          sFirst: "Primero",
+          sLast: "Último",
+          sNext: "Siguiente",
+          sPrevious: "Anterior",
+        },
+      },
+    });
+    repsvmTableSpinner.style.display = "none";
+  }
+  
+  // Recargar la pagina esta página sin parámetros
+  function recargarSinParametros() {
+    window.location.href = actualUrl;
+  }
 
-// Si se recibio el nombre
-if (nombre != null) {
-  // Mostrar el card con la tabla DataTable
-  repsvmFormCard.style.display = "none";
-  repsvmTableCard.style.display = "block";
-  consultarRepsvm(nombre);
-} else {
-  // Mostrar el card con el formulario para consultar
-  repsvmFormCard.style.display = "block";
-  repsvmFormSpinner.style.display = "none";
-  repsvmForm.style.display = "block";
-  consultarButton.addEventListener("click", (thisEvent) => {
-    recargarConParametros();
+  // Recargar la pagina con los parametros del formulario
+  function recargarConParametros() {
+    const elNombre = nombreInput.value;
+    const elDistrito = distritoSelect.value;
+    window.location.href = actualUrl + "?distrito_clave=" + elDistrito + "&nombre=" + elNombre;
+  }
+
+  // Limpiar los valores del formulario y regresar a consultar  
+  function regresarConsulta(){
+    nombreInput.value = "";
+
+    recargarSinParametros();  
+  }
+
+  //
+  // Proceso inicial
+  //
+
+  // Obtener los parametros de la URL
+  const urlParams = new URLSearchParams(window.location.search);
+  var nombre = urlParams.get("nombre");
+  var distrito_clave = urlParams.get("distrito_clave");
+
+  // Si se recibio el nombre
+  if (nombre != null) {
+    // Mostrar el card con la tabla DataTable
+    repsvmFormCard.style.display = "none";
+    repsvmTableCard.style.display = "block";
+    consultarRepsvm(nombre, distrito_clave);
+  } else {
+    // Mostrar el card con el formulario para consultar
+    repsvmFormCard.style.display = "block";
+    repsvmFormSpinner.style.display = "none";
+    repsvmForm.style.display = "block";
+    consultarDistritos();
+    consultarButton.addEventListener("click", (thisEvent) => {
+      recargarConParametros();
+    });
+  }
+  
+  // Realizar evento click para regresar y limpiar url
+  regresarButton.addEventListener("click", (thisEvent) => {
+    regresarConsulta();
   });
-}
+});
